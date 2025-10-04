762a06a1c796603ae74f502a1dab3ef8973c92f3
YuraDnistriansky <yura.dniester@gmail.com>
2025-10-04 18:27:43 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,4 @@
+from project.generator_load import start_generarur_load
+
+if __name__ == "__main__":
+    start_generarur_load()
---
+++ project/generator_load.py
@@ -0,0 +1,2 @@
+def start_generarur_load():
+    print("Start generator load")
